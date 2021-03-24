80c5aa97724e86cc280838c9a69d5933cede3da3
NymaXx <53951684+NymaXx@users.noreply.github.com>
2021-03-24 15:02:23 -0500

    no funciona el cambio de temas asi que lo hare con numeros

--- app/src/main/java/com/example/parcial1_ea_android/MainActivity.java
+++ app/src/main/java/com/example/parcial1_ea_android/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.parcial1_ea_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button greenBtn;
+    private Button yellowBtn;
+    private Button redBtn;
+    private EditText posXt;
+    private EditText posYt;
+    private EditText recordatorio;
+    private Button vistaBtn;
+    private Button confirmarBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        greenBtn = findViewById(R.id.greenBtn);
+        yellowBtn = findViewById(R.id.yellowBtn);
+        redBtn = findViewById(R.id.yellowBtn);
+        posXt = findViewById(R.id.posXt);
+        posYt = findViewById(R.id.posYt);
+        recordatorio = findViewById(R.id.recordatorio);
+        vistaBtn = findViewById(R.id.vistaBtn);
+        confirmarBtn = findViewById(R.id.vistaBtn);
+
     }
 }
